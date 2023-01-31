9ccd0560443735653f3e47d2369e9508214d4e28
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-31 22:29:43 +0400

    feat: to-do-list monday_1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
-import Todolist, {TaskType} from "./TodoList";
+import Todolist, { TaskType } from "./TodoList";
 
 function App() {
-    const toDoListTitle = 'What to learn?';
-    const tasks: Array<TaskType> = [
-        {id: 1, title: 'HTML & CSS', isDone: true},
-        {id: 2, title: 'ES6 & TS', isDone: true},
-        {id: 3, title: 'React & Redux', isDone: false}
-    ]
-    return (
-        <div className="App">
-            <Todolist title={toDoListTitle} tasks={tasks}/>
-        </div>
-    );
+	const toDoListTitle = 'What to learn?';
+	const tasks: Array<TaskType> = [
+		{ id: 1, title: 'HTML & CSS', isDone: true },
+		{ id: 2, title: 'JavaScript', isDone: true },
+		{ id: 3, title: 'React & TS', isDone: false },
+		{ id: 4, title: 'Redux', isDone: false },
+	]
+	return (
+		<div className="App">
+			<Todolist title={toDoListTitle} tasks={tasks} />
+		</div>
+	);
 }
 
 export default App;
