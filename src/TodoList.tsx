088b3c8e73cc60afa91601f5a7f962194f47883c
alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-02-13 20:55:05 +0400

    upd: mon_3 on change way

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, RefObject, useRef } from 'react';
+import React, { FC, RefObject, useRef, useState } from 'react';
 import { FilterValueType } from './App';
 import TasksList from './TasksList';
 
@@ -17,19 +17,23 @@ export type TaskType = {
 }
 
 const TodoList: FC<TodoListPropsType> = (props): JSX.Element => {
-	const addTaskInput: RefObject<HTMLInputElement> = useRef(null);
-	const addTask = () => {
-		if (addTaskInput.current) {
-			props.addTask(addTaskInput.current.value);
-			addTaskInput.current.value = '';
-		}
-	}
+	// const addTaskInput: RefObject<HTMLInputElement> = useRef(null);
+	// const addTask = () => {
+	// 	if (addTaskInput.current && addTaskInput.current.value.length) {
+	// 		props.addTask(addTaskInput.current.value);
+	// 		addTaskInput.current.value = '';
+	// 	}
+	// }
+	const [title, setTitle] = useState<string>('');
 	return (
 		<div className={"todolist"}>
 			<h3>{props.title}</h3>
 			<div>
-				<input ref={addTaskInput} />
-				<button onClick={addTask}>+</button>
+				<input onChange={(e) => setTitle(e.currentTarget.value)} />
+				<button onClick={() => { }}>+</button>
+				{title.length > 15 && <span>Too long</span>}
+				{/* <input ref={addTaskInput} />
+				<button onClick={addTask}>+</button> */}
 			</div>
 			<TasksList tasks={props.tasks} removeTask={props.removeTask} />
 			<div>
